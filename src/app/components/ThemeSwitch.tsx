d2377b7acf3bc92a234ac1ad0cade471843de028
Imran-s-heikh <imransheikhsadi@gmail.com>
2024-08-31 10:55:30 +0600

    fix: Showing wrong Icon based on Theme

--- src/app/components/ThemeSwitch.tsx
+++ src/app/components/ThemeSwitch.tsx
@@ -3,16 +3,20 @@
 import IconMoon from "@/assets/icons/IconMoon";
 import IconSun from "@/assets/icons/IconSun";
 import Hide from "@/components/Hide";
+import useIsMounted from "@/hooks/useIsMounted";
 import { useTheme } from "next-themes";
 import React from "react";
 
 function ThemeSwitch() {
   const { resolvedTheme: theme, setTheme } = useTheme();
   const isDark = theme === "dark";
+  const isMounted = useIsMounted();
 
   function toggleTheme() {
     setTheme(isDark ? "light" : "dark");
   }
+
+  if (!isMounted) return null;
 
   return (
     <button onClick={toggleTheme} className="active:opacity-50 group">
---
+++ src/hooks/useIsMounted.ts
@@ -0,0 +1,14 @@
+import React from "react";
+
+function useIsMounted() {
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false);
+  }, []);
+
+  return isMounted;
+}
+
+export default useIsMounted;
